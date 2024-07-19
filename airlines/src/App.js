13fa22b2c91191d787ff0b09876a422b5c52a908
javahuhu <cgelera3@gmail.com>
2024-07-19 15:24:43 +0800

    change the <p>

--- airlines/src/App.js
+++ airlines/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ughh <code>src/App.js</code> and save to reload.
+          Welcome just Wanna try the update github
         </p>
         <a
           className="App-link"
